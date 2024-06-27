7930237a861679785654c9224fd9b5f8f6021278
Gairaud Alexis <a.gairaud@catie.fr>
2024-06-27 09:35:11 +0200

    Add example of Initialization

--- main.cpp
+++ main.cpp
@@ -3,12 +3,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #include "mbed.h"
+#include "st25r95.h"
 
 namespace {
 #define HALF_PERIOD 500ms
 }
 
 static DigitalOut led1(LED1);
+SPI spi(SPI1_MOSI, SPI1_MISO, SPI1_SCK);
+DigitalOut cs(SPI1_CS);
+DigitalOut irq_n(PWM1_OUT);
+ST25R95 st25r95(&spi, &cs, &irq_n);
 
 int main()
 {
