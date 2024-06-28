ef7b1e364f31419fd01bae0c04dfe32337e1ea49
Gairaud Alexis <a.gairaud@catie.fr>
2024-06-28 15:16:45 +0200

    Example with new feature

--- main.cpp
+++ main.cpp
@@ -14,15 +14,20 @@ SPI spi(SPI1_MOSI, SPI1_MISO, SPI1_SCK);
 DigitalOut cs(SPI1_CS);
 DigitalOut irq_n(PWM1_OUT);
 ST25R95 st25r95(&spi, &cs, &irq_n);
-
+char test[17];
 int main()
 {
     st25r95.initialize();
-    st25r95.select_14443_A_protocol();
+    st25r95.select_14443_a_protocol();
     while (true) {
-        st25r95.get_tag_value();
-        led1 = !led1;
-        if (led1) {
+        if (st25r95.get_tag_value(test) != 0)
+        {
+            for (size_t i = 0; i < 4; i++)
+            {
+                printf("%02X ",test[i]);
+            }
+            printf("\n");
         }
+        led1 = !led1;
     }
 }
